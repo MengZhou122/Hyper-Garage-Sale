04ddb85ca0511a18981aa9b8ea59ca4a5523bf73
Meng Zhou <zhoumeng122@gmail.com>
2020-04-07 20:51:07 -0700

    Correct a typo

---
+++ lib/components/post_data.dart
@@ -0,0 +1,14 @@
+class PostData {
+  String user = '';
+  String category = '';
+  String brand = '';
+  String name = '';
+  String price = '';
+  String year = '';
+  String description = '';
+  String address = '';
+  String picture1 = '';
+  String picture2 = '';
+  String picture3 = '';
+  String picture4 = '';
+}
---
+++ lib/components/text_card.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+class TextCard extends StatelessWidget {
+  final String label;
+  final bool price;
+  final bool description;
+  final Function textIn;
+  TextCard({
+    @required this.label,
+    @required this.textIn,
+    this.price,
+    this.description,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      showCursor: true,
+      autofocus: true,
+      minLines: description == true ? 3 : 1,
+      maxLines: description == true ? 10 : 1,
+      onChanged: textIn,
+      decoration: InputDecoration(
+        prefixIcon: price == true
+            ? Icon(
+                FontAwesomeIcons.dollarSign,
+                size: 17.0,
+              )
+            : null,
+        border: OutlineInputBorder(),
+        labelText: label,
+      ),
+    );
+  }
+}
