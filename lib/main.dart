68d73cca2edf392033db1d492b5ef45bc056c7c8
Meng Zhou <zhoumeng122@gmail.com>
2020-04-17 10:42:38 -0700

    Add navigation logic diagram

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class HyperGS extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      initialRoute: WelcomeScreen.id, //CategoryScreen.id,
+      initialRoute: CategoryScreen.id, //CategoryScreen.id,
       routes: {
         WelcomeScreen.id: (context) => WelcomeScreen(),
         LoginScreen.id: (context) => LoginScreen(),
